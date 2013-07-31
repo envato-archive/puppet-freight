a57ff69289b2558d968a24dddb34044161e0a237
David Goodlad <david@goodlad.net>
2013-07-31 13:55:14 +1000

    Document the freight class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,35 @@
 # == Class: freight
 #
-# Full description of class freight here.
+# Installs freight, "A modern take on the Debian archive"
+#
+# See https://github.com/rcrowley/freight
 #
 # === Parameters
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
+# [*gpgkey*]
+#   The userid of the GPG key you want to use to sign packages
+#
+# [*varlib*]
+#   Where freight stores internal state
+#   Defaults to '/var/lib/freight'
+#
+# [*varcache*]
+#   Where freight puts its public repository - this should be your web server's
+#   docroot
+#   Defaults to '/var/cache/freight'
+#
+# [*origin*]
+#   The (optional, free-text) 'Origin' field of the debian Release file
+#   Defaults to 'Freight'
+#
+# [*label*]
+#   The (optional, free-text) 'Label' field of the debian Release file
+#   Defaults to 'Freight'
+#
+# [*cache*]
+#   Whether or not to cache the control files after each run. true/false
+#   Enabling this leads to a massive increase in speed after your first run.
+#   Defaults to true
 #
 class freight (
   $gpgkey,
