bff76b29667c98f637b6bbfcf3b3bc138368c400
David Goodlad <david@goodlad.net>
2013-07-31 14:13:59 +1000

    Make system specs do their thing

--- spec/system/basic_spec.rb
+++ spec/system/basic_spec.rb
@@ -3,11 +3,19 @@ require 'spec_helper_system'
 describe 'basic tests' do
   it 'class should work without errors' do
     pp = <<-EOS
-      class { 'freight': }
+      class { 'freight': gpgkey => 'DEADBEEF' }
     EOS
 
     puppet_apply(pp) do |r|
       r.exit_code.should == 2
+    end
+
+    shell 'cat /etc/freight.conf' do |r|
+      r.stdout.should =~ /^GPG="DEADBEEF"$/
+    end
+
+    shell 'sudo freight cache' do |r|
+      r.exit_code.should be_zero
     end
   end
 end
